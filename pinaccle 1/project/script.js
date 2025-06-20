170b058e00416bcdec24a5c8ec2789d2f40b28ff
deykris777 <krishnakumardey452@gmail.com>
2025-06-20 12:12:25 +0530

    Delete pinaccle 1 directory

--- pinaccle 1/project/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("reservation-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you for your reservation! We'll get back to you shortly.");
-});
